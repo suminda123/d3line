5ec7a6d46ff08194165325a045c9974c99e9bf3f
CORETEX\earl.suminda <earl.suminda@coretex.com>
2017-05-10 11:17:48 +1200

    add spedometer

---
+++ speedometer.js
@@ -0,0 +1,39 @@
+(function(){
+	var svg = d3.select("#speedometer")
+		.append("svg:svg")
+		.attr("width", 400)
+		.attr("height", 400);
+	
+	
+	var gauge = iopctrl.arcslider()
+		.radius(120)
+		.events(false)
+		.indicator(iopctrl.defaultGaugeIndicator);
+	gauge.axis().orient("in")
+		.normalize(true)
+		.ticks(12)
+		.tickSubdivide(3)
+		.tickSize(10, 8, 10)
+		.tickPadding(5)
+		.scale(d3.scale.linear()
+			.domain([0, 160])
+			.range([-3*Math.PI/4, 3*Math.PI/4]));
+	
+	var segDisplay = iopctrl.segdisplay()
+		.width(80)
+		.digitCount(6)
+		.negative(false)
+		.decimals(0);
+	
+	svg.append("g")
+		.attr("class", "segdisplay")
+		.attr("transform", "translate(130, 200)")
+		.call(segDisplay);
+	
+	svg.append("g")
+		.attr("class", "gauge")
+		.call(gauge);
+	
+	segDisplay.value(56749);
+	gauge.value(125);
+})();
